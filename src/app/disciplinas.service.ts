359be8e1b74ba6564cb5bd4e204321194d654166
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-09-16 16:18:36 -0300

    fui-ajudado-mas-nao-deu-certo

--- src/app/disciplinas.service.ts
+++ src/app/disciplinas.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
 
+interface Disciplinas{
+  diaSemana: string;
+  horario: string;
+  nome: string;
+  p1: number;
+  p2: number;
+}
+
 @Injectable()
 export class DisciplinasService {
+  list: Array<Disciplinas>;
+  storage: Storage; 
 
-  constructor() { }
+  constructor() {
+    this.storage = window.localStorage;
+    this.list = JSON.parse(this.storage.getItem('disciplinas'));
+  }
 
+  add(title: Disciplinas) {
+    this.list.push(title);
+    this.storage.setItem('disciplinas', JSON.stringify(this.list));
+  }
+  remove(index: number) {
+    this.list.splice(index, 1);
+    this.storage.setItem('disciplinas', JSON.stringify(this.list));
+  }
+  getList() {
+    return this.list;
+  }
 }
